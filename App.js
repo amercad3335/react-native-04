6269345f5d3303811d33a2bee51c259ad092ee01
madnesal <madnesalio@gmail.com>
2023-03-09 20:51:22 -0500

    Se agrego logica de calculadora

--- App.js
+++ App.js
@@ -1,19 +1,47 @@
 import { useState } from 'react';
-import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View, TouchableOpacity } from 'react-native';
 import { Banner } from './components/Banner';
 
 export default function App() {
 
   const [valor1, setValor1] = useState('');
   const [valor2, setValor2] = useState('');
-  const [resultado, setResultado] = useState('');
+  const [resultado, setResultado] = useState(0);
+  const [esValido, setEsValido] = useState(false);
+  const [mensaje, setMensaje] = useState('');
+
+  const calcular = (operador) => {
+    setEsValido(true);
+    let resultado = 0;
+    if (valor1 != "" && valor2 != "") {
+      switch (operador) {
+        case "+":
+          resultado = parseFloat(valor1) + parseFloat(valor2);
+          break;
+        case "-":
+          resultado = parseFloat(valor1) - parseFloat(valor2);
+          break;
+        case "*":
+          resultado = parseFloat(valor1) * parseFloat(valor2);
+          break;
+        case "/":
+          resultado = parseFloat(valor1) / parseFloat(valor2);
+          break;
+      }
+      setResultado(resultado)
+      setMensaje('Calculo realizado correctamenete!')
+      return;
+    }
+    setMensaje('Debe ingresar los 2 valores...')
+    setEsValido(false);
+  }
 
   return (
     <View style={styles.container}>
-      <View style={[styles.container, {flex:1, backgroundColor: 'orange'}]}>
-        <Banner name="calculadora"></Banner>
+      <View style={[styles.container, {flex:1, backgroundColor: 'crimson'}]}>
+        {/* <Banner name="calculadora"></Banner> */}
       </View>
-      <View style={[styles.container, {flex:5, backgroundColor: 'yellowgreen'}]}>
+      <View style={[styles.container, {flex:5, backgroundColor: 'yellow'}]}>
         <Text style={{fontWeight: 'bold'}}>Calculadora Básica</Text>
         <Text style={{ marginTop: 15 }}>Valor 1</Text>
         <TextInput 
@@ -31,9 +59,44 @@ export default function App() {
           value={valor2}
         />
         <Text style={{ marginTop: 15 }}>Resultado</Text>
-        <Text>sdd</Text>
+        <Text style={[styles.inputs, {width: '60%', height: 40}]}>{ resultado.toFixed(1) }</Text>
+
+        <View style={[styles.container, {flexDirection: 'row', gap: 5}]}>
+          <TouchableOpacity
+            style={[{ backgroundColor: 'green'}, styles.btn]}
+            onPress={ () => calcular('+') }
+          >
+            <Text style={styles.textBtn}>+</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={[{ backgroundColor: 'red'}, styles.btn]}
+            onPress={ () => calcular('-') }
+          >
+            <Text style={styles.textBtn}>-</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={[{ backgroundColor: 'orange'}, styles.btn]}
+            onPress={ () => calcular('*') }
+          >
+            <Text style={styles.textBtn}>*</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={[{ backgroundColor: 'purple'}, styles.btn]}
+            onPress={ () => calcular('/') }
+          >
+            <Text style={styles.textBtn}>/</Text>
+          </TouchableOpacity>
+        </View>
+        <TouchableOpacity
+            style={[{ backgroundColor: 'black', marginTop: 20}, styles.btn, styles.btnWidth]}
+          >
+            <Text style={styles.textBtn}>c</Text>
+          </TouchableOpacity>
+          <Text
+          style={{ color: esValido ? 'green' : 'red' }}
+          >{mensaje}</Text>
       </View>
-      <View style={[styles.container, {flex:1, backgroundColor: 'gray'}]}>
+      <View style={[styles.container, {flex:1, backgroundColor: 'green'}]}>
         <Text value={ resultado }></Text>
       </View>
     </View>
@@ -56,5 +119,18 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     margin: 10,
     fontSize: 18
+  },
+  btn: {
+    borderRadius: 10,
+    padding: 10, width: 40,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  btnWidth: {
+    width: '26.8%'
+  },
+  textBtn: {
+    color: 'white',
+    fontWeight: 'bold'
   }
 });
